44275dc447b9ca65d281f7a5c5306449042ae5bb
hp-pc <navyashree811@gmail.com>
2022-09-10 20:19:01 +0530

    Added : Calculate Fare given distance and time

--- src/main/java/com/bridgelabz/main/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/main/CabInvoiceGenerator.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.main;
 
-
 public class CabInvoiceGenerator {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Cab Invoice Generator");
-	}
+	public final int COST_PER_KILOMETER = 10;
+	private final int COST_PER_MINUTE = 1;
 
+	public int calculateFare(int distance, int time) {
+		int totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+		if (totalFare < 5)
+			totalFare = 5;
+		return totalFare;
+	}
 }
